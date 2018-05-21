e4fb17219aa8d92893ed86a0626d05ea5e1351c3
Pedro Marzagao <pedromarzagao@gmail.com>
2018-05-21 11:39:31 +0100

    Spoon project adding links

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # get 'pages/home'
 
   get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/:id', to: 'restaurants#show'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
   post 'restaurants', to: 'restaurants#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
